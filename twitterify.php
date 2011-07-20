53a07fcfaed45b162971b27bbb4d86b451fa8b47
mattsay <mattsay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-20 13:40:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitterify/trunk@412557 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitterify.php
+++ twitterify.php
@@ -3,62 +3,104 @@
 Plugin Name: Twitterify
 Plugin URI: http://shailan.com/wordpress/plugins/twitterify
 Description: Enables use of <strong>autolink</strong>, <strong>#hashtags</strong> and <strong>@author</strong> links on your posts. <strong>Links are not directed to twitter. They provide this functionality on your site.</strong>
-Version: 1.1
+Version: 1.2
 Author: Matt Say
 Author URI: http://shailan.com
 */
 
-function twitterify_content ( $text ){
+global $twitterify;
 
-	// Make content clickable
-	//$text = make_clickable( $text );
+if(!class_exists('stf_twitterify')){
+class stf_twitterify {
+
+	// Constructor
+	function stf_twitterify(){
 	
-	$ret = ' ' . $text;
-    $ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a target=\"_blank\" rel=\"nofollow\" href=\"\\2\" >\\2</a>'", $ret);
-    $ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a target=\"_blank\" rel=\"nofollow\" href=\"http://\\2\" >\\2</a>'", $ret);
-	
-	// Remove http://
-	$ret = preg_replace( '/(">http:\/\/)(.*?)<\/a>/i', "\">$2</a>", $ret ); 
-	
-	 // Remove www.
-	$ret = preg_replace( '/(">www.)(.*?)<\/a>/i', "\">$2</a>", $ret );
-	
-	// Check permalink structure
-	if ( get_option('permalink_structure') != '' ) { 
-	
-		// permalinks enabled
-		$permalink = get_option( 'permalink_structure', '' );
+		// Get permalink structure
+		if ( get_option('permalink_structure') != '' ) { 
+
+			// Permalinks enabled
+			$permalink = get_option( 'permalink_structure', '' );
+			
+			$prefix = '';
+			if( strpos( $permalink, '/index.php/') !== false ){
+				$prefix = '/index.php';
+			}		
+			
+			$tag_base = get_option( 'tag_base', 'tag' );
+			if($tag_base == ''){ $tag_base = 'tag'; }
+			$this->tag_base = $prefix . "/" . $tag_base . "/";
+			$this->author_base = $prefix . "/author/";
+			
+		} else {
+
+			// Permalinks not enabled
+			$this->tag_base = "?tag=";
+			$this->author_base = "?author=";
+			
+		}
 		
-		$prefix = '';
-		if( strpos( $permalink, '/index.php/') !== false ){
-			$prefix = '/index.php';
-		}		
-		
-		$tag_base = get_option( 'tag_base', 'tag' );
-		if($tag_base == ''){ $tag_base = 'tag'; }
-		$tag_base = $prefix . "/" . $tag_base . "/";
-		$author_base = $prefix . "/author/";
-		
-	} else {
-	
-		// permalinks not enabled
-		$tag_base = "?tag=";
-		$author_base = "?author=";
-		
+		add_filter( 'the_content', array( &$this, 'twitterify_content' ), 99, 1 );
+		add_filter( 'the_excerpt', array( &$this, 'twitterify_content' ), 99, 1 );
+
 	}
-	
-    // Author links
-    $twitter = "/ @([A-Za-z0-9_]+)/is";
-    $ret = preg_replace ($twitter, " <a href='" . home_url($author_base) . "$1'>@$1</a>", $ret);
 
-    // Hashtags
-    $hashtag = "/ #([A-Aa-z0-9_-]+)/is";
-    $ret = preg_replace ($hashtag, " <a href='" . home_url($tag_base) . "$1'>#$1</a>", $ret);
-	
-	// Return post content
-    return $ret;
-	
-} 
+	function twitterify_content ( $text ){
+		global $author_base;
 
-add_filter( 'the_content', 'twitterify_content', 99, 1 );
-add_filter( 'the_excerpt', 'twitterify_content', 99, 1 );
+		// Make content clickable
+		//$text = make_clickable( $text );
+		
+		$ret = ' ' . $text;
+		$ret = preg_replace("#(^|[\n> ])([\w]+?://[\w]+[^ \"\n\r\t<]*)#ise", "'\\1<a target=\"_blank\" rel=\"nofollow\" href=\"\\2\" >\\2</a>'", $ret);
+		$ret = preg_replace("#(^|[\n> ])((www|ftp)\.[^ \"\t\n\r<]*)#ise", "'\\1<a target=\"_blank\" rel=\"nofollow\" href=\"http://\\2\" >\\2</a>'", $ret);
+		
+		// Remove http://
+		$ret = preg_replace( '/(">http:\/\/)(.*?)<\/a>/i', "\">$2</a>", $ret ); 
+		
+		 // Remove www.
+		$ret = preg_replace( '/(">www.)(.*?)<\/a>/i', "\">$2</a>", $ret );
+		
+		// Author links
+		$author_pattern = "/([\n> ])@([A-Za-z0-9_]+)/is";
+		$ret = preg_replace_callback ( $author_pattern, array( &$this, 'twitterify_author_callback' ), $ret );
+
+		// Hashtags
+		$hashtag_pattern = "{([^//])#([A-Za-z0-9_-]+)}is";
+		$ret = preg_replace_callback ( $hashtag_pattern, array( &$this, 'twitterify_tag_callback' ), $ret );
+		
+		// Return post content
+		return $ret;
+		
+	} 
+
+	// Check if author exists
+	function twitterify_author_callback( $matches ){
+		global $author_base;
+		
+		if ( username_exists( $matches[2] ) ){
+			return $matches[1] . "<a href='" . home_url( $this->author_base ) . $matches[2] . "'>@" . $matches[2] . "</a>";
+		} else {
+			return $matches[1] . "<a href='http://twitter.com/" . $matches[2] . "'>@" . $matches[2] . "</a>";
+		}
+	}
+
+	// Check tags for color codes
+	function twitterify_tag_callback( $matches ){
+		global $tag_base;
+		
+		// Check fox hex color codes 
+		if( strlen($matches[2]) == 3 || strlen($matches[2]) == 6 ){
+			// Check for chars
+			if( strlen( preg_replace("/[^0-9A-Fa-f]/", '', $matches[2])) == 6 || strlen( preg_replace("/[^0-9A-Fa-f]/", '', $matches[2])) == 3 ){
+				// Surely, hexadecimal value
+				return $matches[1] . "#" . $matches[2];
+			}
+		}
+		
+		return $matches[1] . " <a href='" . home_url( $this->tag_base ) . "". $matches[2] ."'>#".$matches[2]."</a>";
+	}
+
+} } // stf_twitterify
+
+$twitterify = new stf_twitterify();
